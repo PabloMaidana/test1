3be1668ae366130b7df1a5ffb4693296679734ac
usuario <pablomaid1@gmail.com>
2024-03-21 12:10:03 -0300

    nuevo proyecto

--- EDA_ordenamientos/src/eda_ordenamientos/EDA_ordenamientos.java
+++ EDA_ordenamientos/src/eda_ordenamientos/EDA_ordenamientos.java
@@ -2,22 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyectotest;
+package eda_ordenamientos;
 
 /**
  *
  * @author usuario
  */
-public class ProyectoTest {
+public class EDA_ordenamientos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // primer commit tercer commit
-        // cuarto commit
-        System.out.println("quinto commit");
     }
     
 }
