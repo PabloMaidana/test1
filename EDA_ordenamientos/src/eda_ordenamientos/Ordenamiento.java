da2c49318479f84d688c81c315b25437dc33154e
usuario <pablomaid1@gmail.com>
2024-03-21 12:19:03 -0300

    nueva clase

---
+++ EDA_ordenamientos/src/eda_ordenamientos/Ordenamiento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eda_ordenamientos;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ordenamiento {
+    private int[] datos = new int [10000];
+    
+    
+}
